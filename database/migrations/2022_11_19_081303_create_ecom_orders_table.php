043880189e3bf9251f0eed0eca4018f3d3c7b030
Mostafa Mamun Emon <mostafaemon@Mostafas-MacBook-Pro.local>
2022-11-19 14:18:19 +0600

    order start

---
+++ database/migrations/2022_11_19_081303_create_ecom_orders_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ecom_orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('total');
+            $table->integer('delivery_charge');
+            $table->integer('grand_total');
+            $table->dateTime('order_date_time');
+            $table->string('status',20);
+            $table->timestamps();
+
+            $table->index(['status']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ecom_orders');
+    }
+};
